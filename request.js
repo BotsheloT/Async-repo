a84f9c9781fb0f5fc69efef03242f18901124d35
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-02-14 10:30:19 +0200

    Callback and Function addition

--- request.js
+++ request.js
@@ -1,17 +1,28 @@
 /* For this code to work properly, it needs to be run in a browser */
 
-//Request object
-const req = new XMLHttpRequest;
+//Function for request
+let getData = (cFunc) => {
+    //Request object
+    const req = new XMLHttpRequest;
 
-//Event listener
-req.addEventListener('readystatechange', () => {
-    if (req.readyState === 4 && req.status === 200){
-        console.log(req.responseText);
-    } else {
-        console.log('Could not fetch data');
-    }
+    //Event listener
+    req.addEventListener('readystatechange', () => {
+        if (req.readyState === 4 && req.status === 200){
+            cFunc(undefined, req.responseText);
+        } else {
+            cFunc('Could not complete request', undefined);
+        }
+    });
+
+    //Getting API for request
+    req.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+    req.send();
+}
+
+//Calling function
+getData((err, data) =>{
+    console.log('Callback fired');
+    if(err){
+        console.log(err);
+    }else console.log(data);
 });
-
-//Getting API for request
-req.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-req.send();
